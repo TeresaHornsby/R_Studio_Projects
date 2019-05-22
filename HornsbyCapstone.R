298213090683ad15905d542854c518e732bef090
Teresa Hornsby <thornsby@drury.edu>
2019-05-22 14:11:00 -0400

    Add files via upload

--- HornsbyCapstone.R
+++ HornsbyCapstone.R
@@ -1,3 +1,8 @@
+
+#SUMMARY
+#This project is to complete the Movielens capstone assignment for EDX Datascience Program.
+#I tried to run a variety of algorithms but none would work with my computer. I opted for a very simple approach: Penalized Least Squares Regression. I applied this to the movie rating and to the timestamp. My analysis suggested that 
+
 #Download edx dataset
 
 dl <- tempfile()
@@ -36,46 +41,16 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 
 #Explore data
 
-edx %>% separate_rows(genres, sep = "\\|") %>%
-  group_by(genres) %>%
-  summarize(count = n()) %>%
-  arrange(desc(count))
 
-edx %>% group_by(movieId, title) %>%
-  summarize(count = n()) %>%
-  arrange(desc(count))
-#Number of columns in edx set: 
-ncol(edx) 
-#Number of rows in edx set: 
-nrow(edx) 
-#How many films have a rating of 0? 
-sum(edx$rating==0) 
-#How many films have a rating of 3? 
-sum(edx$rating == 3)
-#How many distinct films are in edx?
-edx %>% summarize(n_movies = n_distinct(movieId)) 
-#How many distinct users are there?
-edx %>% summarize(n_users = n_distinct(userId))
-#How many ratings for each of these genres?
-Drama <- edx %>% filter(str_detect(genres,"Drama"))
-Comedy <- edx %>% filter(str_detect(genres,"Comedy"))
-Thriller <- edx %>% filter(str_detect(genres,"Thriller"))
-Romance <- edx %>% filter(str_detect(genres,"Romance")) 
-#What are the five most given ratings in order from most to least?
-edx %>% group_by(title) %>% summarise(number = n()) %>%
-  arrange(desc(number))
-head(sort(-table(edx$rating)),5)
-#Half star ratings are less common than whole ratings:
-table(edx$rating)
-#Convert timestamp to datetime (for future use)
+#Convert timestamp to datetime
 
 edx <- edx %>% mutate(timestamp = as.POSIXct(timestamp, origin = "1970-01-01", 
                                              tz = "GMT"))
-edx$timestamp <- format(edx$timestamp, "%Y")
+edx$timestamp <- format(edx$timestamp, "%H")
 
-colnames(edx)
+#colnames(edx)
 
-names(edx)[names(edx) == "timestamp"] <- "year_rated"
+names(edx)[names(edx) == "timestamp"] <- "hour_rated"
 
 releaseyear <- stringi::stri_extract(edx$title, regex = "(\\d{4})", comments = TRUE) %>% 
   as.numeric()
@@ -84,49 +59,53 @@ edx <- edx %>% mutate(release_year = releaseyear)
 
 validation <- validation %>% mutate(timestamp = as.POSIXct(validation$timestamp, 
                                                            origin = "1970-01-01", tz = "GMT"))
-validation$timestamp <- format(validation$timestamp, "%Y")
+validation$timestamp <- format(validation$timestamp, "%H")
 
 colnames(validation)
-names(validation)[names(validation) == "timestamp"] <- "year_rated"
+names(validation)[names(validation) == "timestamp"] <- "hour_rated"
 
 releaseyear2 <- stringi::stri_extract(validation$title, regex = "(\\d{4})", 
                                       comments = TRUE) %>% as.numeric()
 
 validation <- validation %>% mutate(release_year = releaseyear2)
+#This next section is an excellent idea; not mine. I tried lms on this; knn, etc. I tried this method
+#with the time of day (thinking those ratings made while one might be drunk would be higher). I tried 
+#every variable. This one works). But it's not my idea. 
 RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings - predicted_ratings)^2))
 }
 
-Lambdas <- seq(0, 5, 0.25)
+lambdas <- seq(0, 5, 0.25)
 
-Rmses <- sapply(lambdas,function(l){
+rmses <- sapply(lambdas,function(l){
   
-#Determine the mean of ratings from the edx training set
+  #Calculate the mean of ratings from the edx training set
   mu <- mean(edx$rating)
   
-#Adjust with low ratings
+  #Adjust mean by movie effect and penalize low number on ratings
   b_i <- edx %>% 
     group_by(movieId) %>%
     summarize(b_i = sum(rating - mu)/(n()+l))
+  
+  #ajdust mean by user and movie effect and penalize low number of ratings
   b_u <- edx %>% 
     left_join(b_i, by="movieId") %>%
     group_by(userId) %>%
     summarize(b_u = sum(rating - b_i - mu)/(n()+l))
-   
   
-#Use the training set to find the best predicted lambda 
-  predictedRating<- 
+  #predict ratings in the training set to derive optimal penalty value 'lambda'
+  predicted_ratings <- 
     edx %>% 
     left_join(b_i, by = "movieId") %>%
     left_join(b_u, by = "userId") %>%
     mutate(pred = mu + b_i + b_u) %>%
     .$pred
   
-  return(RMSE(predictedRating,edx$rating))
+  return(RMSE(predicted_ratings, edx$rating))
 })
 
-lambda <-Lambdas[which.min(Rmses)]
-paste('The best Lambda of',min(Rmses),'is with Lambda',lambda)
+lambda <- lambdas[which.min(rmses)]
+paste('Optimal RMSE of',min(rmses),'is achieved with Lambda',lambda)
 
 #Run the same program with best lambda
 lambda <- 0.5
@@ -143,19 +122,19 @@ predict_a<- sapply(lambda,function(l){
   
 #Find user effect with best lambda
   b_u <- edx %>% 
-    left_join(b_i, by="movieId") %>%
-    group_by(userId) %>%
+    left_join(b_i, by="userId") %>%
+    group_by(hour_rated) %>%
     summarize(b_u = sum(rating - b_i - mu)/(n()+l))
   
 #Predict the ratings on the validation set
   predictedRating<- 
     validation %>% 
-    left_join(b_i, by = "movieId") %>%
-    left_join(b_u, by = "userId") %>%
+    left_join(b_i, by = "userId") %>%
+    left_join(b_u, by = "hour_rated") %>%
     mutate(pred = mu + b_i + b_u) %>%
     .$pred #validation
   
   return(predictedRating)
-  
+#this is the end of "not mine"   
 })
 
